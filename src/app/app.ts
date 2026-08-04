82e17c55d35be6d0a90a9bfa4adae72783a587eb
EmadAdel22 <emadadelgabar@gmail.com>
2026-08-04 12:02:24 +0300

    Add Get All Categories

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
-export class App {
-  protected readonly title = signal('Client');
+export class App implements OnInit {
+
+  BaseUrl = 'https://localhost:7238/api/Categories/get-all';
+
+  // Signal بدل any
+  Category = signal<any[]>([]);
+
+  constructor(private http: HttpClient) {}
+
+  getCategories() {
+    this.http.get<any[]>(this.BaseUrl).subscribe({
+      next: (value) => {
+        this.Category.set(value);
+        console.log(value);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
 }
